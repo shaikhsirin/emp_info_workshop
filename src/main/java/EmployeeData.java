a5a8fe001600e551dabcf936e9a318f412f3eb35
shaikhsirin <shaikhsirin8080example.com>
2022-04-21 09:43:23 +0530

    Added:- Create Employee Data using JDBC

---
+++ src/main/java/EmployeeData.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class EmployeeData {
+    /**
+     * variables
+     */
+    public int id;
+    public String name;
+    public double salary;
+    public LocalDate start;
+
+    /**
+     * here using getter setter method,
+     * The get method returns the vaiable value and set method set the value.
+     */
+
+    /**
+     * The get method returns the value of the variable id
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+}
